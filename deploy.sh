cb208a7b61b3d0fe2aa725323e238ed54779b4ba
simonzhangs <2863389578@qq.com>
2022-09-18 22:41:33 +0800

    deploy: change script

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,23 @@
 #!/usr/bin/env sh
 # 确保脚本抛出遇到的错误
 set -e
-npm run build # 生成静态文件
-cd docs/.vuepress/dist # 进入生成的文件夹
 
-# deploy to github
-echo 'songbenblog.com' > CNAME
-if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy'
-  githubUrl=git@github.com:simonzhangs/awesome-fe-interview.git
-  msg='来自github action的自动部署'
-  githubUrl=https://simonzhangs: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@github.com/simonzhangs/awesome-fe-interview.git
-  git config --global user.name "simonzhangs"
-  git config --global user.email "2863389578@qq.com"
-fi
+# 生成静态文件
+# npm run build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# Set CNAME for "gh-pages" branch
+echo 'songbenblog.com' > CNAME  # 改成你要绑定的域名
+
+msg='deploy'
+githubUrl=git@github.com:simonzhangs/awesome-fe-interview.git  # 按你的代码仓库信息进行修改
+
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master  # 推送到github 主分支
-# git push -f $githubUrl master:gh-pages # 推送到github
+git push -f $githubUrl master # 推送到github
 
-cd -
+cd - # 退回开始所在目录
 rm -rf docs/.vuepress/dist
